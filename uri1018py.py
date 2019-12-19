77e606ae79577e293870adca21f3228279ccb272
HKmahim <mdmahimhossen134@gmail.com>
2019-12-19 19:01:12 +0600

    Py

---
+++ uri1018py.py
@@ -0,0 +1,16 @@
+list = [100,50,20,10,5,2,1]
+n = int(input())
+print(n)
+
+
+
+for i in range(0,7):
+    c = n//list[i]
+    n=(n-(c*list[i]))
+    l = list[i]
+    #print("%d nota(s) de R$ %d,00"%c%l)
+    res = "{} nota(s) de R$ {},00"
+    result = res.format(c,l)
+    print(result)
+    
+
